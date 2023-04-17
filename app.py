bd48d21ff6d36e8ef03530a9e612e405d89038b4
Rhi Sehl <rhisehl@gmail.com>
2023-04-17 17:46:40 -0600

    Update Json results

--- app.py
+++ app.py
@@ -32,4 +32,4 @@ def index():
 
 @app.route('/api/get_all')
 def getdata():
-    return jsonify(**Activities,**CountryFlags,**Locations,**SunHours,**Temperature,**ActivityList)
+    return jsonify(**CountryFlags**ActivityList)
