8c7f8b840cd4177dd14f68cfc46c608e0279dbb5
Rhi Sehl <rhisehl@gmail.com>
2023-04-17 16:06:19 -0600

    updating logic

--- static/js/logic.js
+++ static/js/logic.js
@@ -2,56 +2,83 @@
 const url = "";
 
 // Initialize Map
-var myMap = L.map("map",{
-    center: [
-                    37.09, -95.71,
-                ],
-                zoom: 1.5,
-  });
+function initMap(allData){
+    var myMap = L.map("map",{
+        center: [
+                        37.09, -95.71,
+                    ],
+                    zoom: 1.5,
+      });
+    // add tile layer
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(myMap);
+   // create markers for all datapoints
+    L.geoJSON(allData,{
+        // create the points
+        pointToLayer: function(feature,latlng){
+            return L.marker(latlng);
+        },
+        onEachFeature: function(feature,layer){
+            // Update the top activities based on the location that is clicked
+            layer.on({
+                click: function(event){
+                    updateActivities(event.target());
+                },
+            });
+            // Create a popup for each location with relevant info
+            layer.bindPopup("hi!")////////INSERT POPUP INFO HERE////////)  
+        },
+    }).addTo(myMap);
+    
 
-// add tile layer
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(myMap);
 
 
 // update chart when dropdown is changed
-function updateChart(){
+function updateCharts(){
+    // collect the new values of all dropdowns
     let monthDropdown = d3.select('#monthSelect').property("value");
     let costDropdown = d3.select('#costSelect').property("value");
     let tempDropdown = d3.select('#tempSelect').property("value");
     let sunDropdown = d3.select('#sunSelect').property("value");
-    //// add 
-}
+    // 
+};
 
 // initialize top destinations
+function initDestinations(){
 
+};
 
 // initialize top activities
 
+function initActivities(){
+
+};
 
 
+// update top activities
+function updateActivities(){
 
-
+};
 
 /////////////////////////////////////////////////////////////////////////
 // Create function for changed month
 function monthChanged(){
-    updateChart()
+    updateCharts()
 };
 
 // Create function for changed cost
 function costChanged(){
-    updateChart()
-}
+    updateCharts()
+};
 // Create function for changed temp
 function tempChanged(){
-    updateChart()
-}
+    updateCharts()
+};
 // Create function for changed sunshine
 function sunChanged(){
-    updateChart()
-}
+    updateCharts()
+};
 
 //////////////////////////////////////////////////////////////////////////
 // data promise
@@ -59,10 +86,11 @@ d3.json(url).then(function(data){
     // confirm data loaded successfully
     console.log(data);
     // separate out needed data
-
+    allData = ;
     // initialize map
-
+    initMap(allData);
     // initialize top destinations
-
+    initDestinations(allData);
     // initialize top activities
+    initActivities(allData);
 });
