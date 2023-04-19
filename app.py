6acd72967c0e12cebc50f3fcd1fe457237f2499b
Rhi Sehl <rhisehl@gmail.com>
2023-04-18 18:47:05 -0600

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from sqlalchemy.ext.automap import automap_base
 from flask_cors import cross_origin
 
 engine = create_engine(os.getenv('DATABASE_URI'))
-MetaData.clear()
+MetaData.clear(self)
 MetaData.create_all()
 
 Base = automap_base()
